bc6654d96fbb95e56ae56039772dc145966110ef
Arley <arley.dev>
2023-06-24 18:27:21 -0500

    fixed animation on description

--- script.js
+++ script.js
@@ -13,31 +13,21 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-const section = document.getElementById('about-me');
+const section = document.getElementById('description');
 
-// Obtener la posición superior de la sección
 const sectionTop = section.offsetTop;
 
-// Función para verificar si la sección está visible en la ventana
 function checkSectionVisibility() {
     const windowHeight = window.innerHeight;
     const scrollY = window.scrollY || window.pageYOffset;
 
-    // Calcular la posición inferior de la sección
     const sectionBottom = sectionTop + section.offsetHeight;
 
-    // Verificar si la sección está visible en la ventana
     if (scrollY + windowHeight > sectionTop && scrollY < sectionBottom) {
-        // Agregar la clase para activar la animación
-        section.classList.add('animate-section');
-    } else {
-        // Remover la clase de la animación
-        section.classList.remove('animate-section');
+        section.classList.add('description');
     }
 }
 
-// Agregar el evento de scroll para verificar la visibilidad de la sección
 window.addEventListener('scroll', checkSectionVisibility);
 
-// Verificar la visibilidad de la sección al cargar la página
 checkSectionVisibility();
