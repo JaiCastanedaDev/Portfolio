db03969338b0514df5b4b27a10eba52a5f994ad2
Arley <arley.dev>
2023-06-24 18:18:49 -0500

    about me updated

--- script.js
+++ script.js
@@ -9,39 +9,35 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
-document.addEventListener("DOMContentLoaded", function() {
-    var text = "Soy un apasionado desarrollador de software, constantemente inmerso en el mundo de la tecnología. Siempre estoy buscando oportunidades para" +
-        " aprender y crecer en mi campo, explorando nuevas tecnologías y enfoques. Mi pasión por el desarrollo de software me impulsa a mantenerme actualizado" +
-        " y adquirir nuevas habilidades, con el objetivo de crear soluciones innovadoras y eficientes.";
-    var typedText = document.getElementById("typed-description");
-    var index = 0;
-
-    function type() {
-        if (index < text.length) {
-            typedText.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, 10);
-        }
-    }
-
-    var aboutMeSection = document.getElementById("about-me");
-    var options = {
-        rootMargin: "0px",
-        threshold: 0.5
-    };
-
-    var observer = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                type();
-                observer.unobserve(entry.target);
-            }
-        });
-    }, options);
-
-    observer.observe(aboutMeSection);
-});
 function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+const section = document.getElementById('about-me');
+
+// Obtener la posición superior de la sección
+const sectionTop = section.offsetTop;
+
+// Función para verificar si la sección está visible en la ventana
+function checkSectionVisibility() {
+    const windowHeight = window.innerHeight;
+    const scrollY = window.scrollY || window.pageYOffset;
+
+    // Calcular la posición inferior de la sección
+    const sectionBottom = sectionTop + section.offsetHeight;
+
+    // Verificar si la sección está visible en la ventana
+    if (scrollY + windowHeight > sectionTop && scrollY < sectionBottom) {
+        // Agregar la clase para activar la animación
+        section.classList.add('animate-section');
+    } else {
+        // Remover la clase de la animación
+        section.classList.remove('animate-section');
+    }
+}
+
+// Agregar el evento de scroll para verificar la visibilidad de la sección
+window.addEventListener('scroll', checkSectionVisibility);
+
+// Verificar la visibilidad de la sección al cargar la página
+checkSectionVisibility();
